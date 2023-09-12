3cc74879d239b07641fd8a42c97b120c9d7a940f
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-12 15:48:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Switch_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter a number between 1 and 3");
+            int number = int.Parse(Console.ReadLine());//convert to an integer
+
+            switch (number) 
+            {
+                case 1: 
+                    Console.WriteLine("You have entered 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You have entered 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You have entered 3");
+                    break;
+                default: Console.WriteLine("You broke rules");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
